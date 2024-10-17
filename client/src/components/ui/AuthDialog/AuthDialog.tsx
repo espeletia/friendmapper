00aeea8aae36178486689cc5d4554040859b768f
Michal Vaniš <zirixcz@gmail.com>
2024-10-17 17:40:45 +0200

    client/fix: Fixes

--- client/src/components/ui/AuthDialog/AuthDialog.tsx
+++ client/src/components/ui/AuthDialog/AuthDialog.tsx
@@ -2,25 +2,31 @@ import React from "react";
 import InputField from "./InputField";
 import styles from "./AuthDialog.module.css";
 import CustomButton from "./CustomButton";
-
+import { useNavigate } from "react-router-dom";
 
 const AuthDialog = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <div className={styles['justify-center']}>
+    <div className={styles.center}>
+      <div className={styles["justify-center"]}>
         <h1 className={styles.heading}>
           Friends<span className={styles.hk}>HK</span>
         </h1>
-        <InputField type="email" placeholder="Email" title="Email"/>
-        <InputField type="password" placeholder="Heslo" title= "Heslo"/>
+        <InputField type="email" placeholder="Email" title="Email" />
+        <InputField type="password" placeholder="Heslo" title="Heslo" />
         <div className={styles.gap}></div>
         <CustomButton text={"Příhlásit se"} onClick={() => null}></CustomButton>
         <div className={styles.smallGap}></div>
-        <CustomButton text={"Zaregistrovat"} onClick={() => null} backgroundColor="#F0EFFF" textColor="#1B1937" hoverColor="#DEDEEC"></CustomButton>
+        <CustomButton
+          text={"Zaregistrovat"}
+          onClick={() => navigate("/auth/register")}
+          backgroundColor="#F0EFFF"
+          textColor="#1B1937"
+          hoverColor="#DEDEEC"
+        ></CustomButton>
       </div>
     </div>
   );
 };
 
 export default AuthDialog;
-
--- client/src/components/ui/AuthDialog/RegisterDialog.tsx
+++ client/src/components/ui/AuthDialog/RegisterDialog.tsx
@@ -3,28 +3,42 @@ import InputField from "./InputField";
 import styles from "./AuthDialog.module.css";
 import CustomButton from "./CustomButton";
 import ProfilePicturePlaceholder from "./ProfilePicturePlaceholder";
-
+import { useNavigate } from "react-router-dom";
 
 const RegisterDialog = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <div className={styles['justify-center']}>
+    <div className={styles.center}>
+      <div className={styles["justify-center"]}>
         <h1 className={styles.heading}>
           Friends<span className={styles.hk}>HK</span>
         </h1>
         <ProfilePicturePlaceholder />
-        <InputField type="text" placeholder="Zadejte vaše jméno..." title="Jméno"/>
-        <InputField type="email" placeholder="Zadejte email" title="Email"/>
-        <InputField type="password" placeholder="Zadejte heslo..." title= "Heslo"/>
-        <InputField type="password" placeholder="Zadejte heslo..." title= "Heslo (znovu)"/>
+        <InputField
+          type="text"
+          placeholder="Zadejte vaše jméno..."
+          title="Jméno"
+        />
+        <InputField type="email" placeholder="Zadejte email" title="Email" />
+        <InputField
+          type="password"
+          placeholder="Zadejte heslo..."
+          title="Heslo"
+        />
+        <InputField
+          type="password"
+          placeholder="Zadejte heslo..."
+          title="Heslo (znovu)"
+        />
         <div className={styles.gap}></div>
-        <CustomButton text={"Zaregistrovat se"} onClick={() => null}></CustomButton>
+        <CustomButton
+          text={"Zaregistrovat se"}
+          onClick={() => navigate("/map")}
+        ></CustomButton>
         <div className={styles.smallGap}></div>
-        
       </div>
     </div>
   );
 };
 
 export default RegisterDialog;
-
